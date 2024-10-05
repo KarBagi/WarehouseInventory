883f55fc67ad438bfc2c524549793a7d97d14384
karol <karolb98@gmail.com>
2024-10-05 18:48:24 +0200

    dockerignore

--- Program.cs
+++ Program.cs
@@ -6,36 +6,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<WarehouseInventoryContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("WarehouseInventoryContext") ?? throw new InvalidOperationException("Connection string 'WarehouseInventoryContext' not found.")));
 
-// Dodaj usługi kontrolerów do kontenera
 builder.Services.AddControllers();
 
-// Dodaj usługę Swaggera do kontenera
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
     {
-        Version = "v1",
-        Title = "My API",
-        Description = "Opis mojej API",
+        Version = "v1.2",
+        Title = "Warehouse Inventory API",
+        Description = "",
     });
 });
 
 var app = builder.Build();
 
-// Konfiguracja Swaggera i interfejsu Swagger UI tylko w środowiskach deweloperskich
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "WarehouseInventory v1.2");
     });
 }
 
 app.UseHttpsRedirection();
 
-// Dodaj mapowanie kontrolerów
 app.MapControllers();
 
 app.Run();
